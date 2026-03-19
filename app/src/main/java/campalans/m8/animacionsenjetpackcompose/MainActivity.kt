111143a76b995c45b4015638ae96173edc06fb6b
Ferran <ferranmusan@gmail.com>
2026-03-19 19:06:58 +0100

    Algo

--- app/src/main/java/campalans/m8/animacionsenjetpackcompose/MainActivity.kt
+++ app/src/main/java/campalans/m8/animacionsenjetpackcompose/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import campalans.m8.animacionsenjetpackcompose.ui.theme.AnimacionsEnJetpackComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +23,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AnimacionsEnJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
                     )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    var visible by remember { mutableStateOf(true) }
+
+    Column {
+        Button(onClick = { visible = !visible }) {
+            Text(if (visible) "Hide" else "Show")
+        }
+
+        AnimatedVisibility(visible) {
+            Text("This text animates in/out")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AnimacionsEnJetpackComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
